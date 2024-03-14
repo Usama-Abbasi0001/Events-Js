896e57c9945e4ff9df56d7ed5ef7079e5b026666
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-03-14 10:02:17 +0500

    Events

---
+++ Events.js
@@ -0,0 +1,15 @@
+function hello(){
+    document.write("Hello World ")
+}
+function hy(){
+    alert("han bhai samhaj a raha han")
+}
+
+function You(){
+    alert("On Mouse Enter Click")
+}
+
+function me(){
+    document.write("On Mouse Out Click")
+}
+
